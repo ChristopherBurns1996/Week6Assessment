7949f635879571202e12396cfbf4120ddadb3754
BMarcysiak <bartekmarcysiak@gmail.com>
2019-09-13 11:25:46 +0100

    Created backend framework

--- src/main/java/com/qa/steamwishlistbackend/repositories/SteamGameRepository.java
+++ src/main/java/com/qa/steamwishlistbackend/repositories/SteamGameRepository.java
@@ -3,5 +3,8 @@ package com.qa.steamwishlistbackend.repositories;
 import com.qa.steamwishlistbackend.entities.SteamGame;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface SteamGameRepository extends CrudRepository<SteamGame, Integer> {
+    Optional<SteamGame> findByName(String name);
 }
